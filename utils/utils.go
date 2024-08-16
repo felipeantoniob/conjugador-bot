f24175849dd34b0eba9d173ff778b9060cb41360
felipeantoniob <73531907+felipeantoniob@users.noreply.github.com>
2024-08-16 15:36:51 +0800

    Add utils package

---
+++ utils/utils.go
@@ -0,0 +1,23 @@
+package utils
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+// WaitForShutdown sets up a channel to listen for specific shutdown signals and blocks until one of those signals is received.
+func WaitForShutdown() {
+	// Create a channel to receive OS signals
+	sc := make(chan os.Signal, 1)
+
+	// Notify the channel for specific shutdown signals
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+
+	// Block until one of the shutdown signals is received
+	sig := <-sc
+
+	// Print the received signal (for debugging or logging purposes)
+	fmt.Printf("Received shutdown signal: %v\n", sig)
+}
